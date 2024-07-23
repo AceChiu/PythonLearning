5e0b1c968f73dec4bb35f70ac61625e5b2b152f5
AceChiu <taiwango.01g@g2.nctu.edu.tw>
2024-07-22 18:06:13 -0700

    datatype

---
+++ datatype.py
@@ -0,0 +1,21 @@
+#number
+3456
+3.56
+#string
+"This is a sentence"
+#boolean
+True
+False
+#List: in-order and variable
+[3,4,5]
+["Hello", "World"]
+#Tuple: in-order and immutable
+(3,4,5)
+("Hello", "World")
+#Set: non-order
+{3,4,5}
+#Dictionary: key-value
+{"apple": "蘋果", "data": "資料"}
+#variable = data
+x=3
+print(x)
